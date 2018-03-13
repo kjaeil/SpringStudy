df7e99a75511450e42cf5b9c85e62e6b8c4b030b
kjaeil <rlawodlf92@naver.com>
2018-03-13 18:20:19 +0900

    weather update

--- src/main/java/com/example/hw/controller/PostController.java
+++ src/main/java/com/example/hw/controller/PostController.java
@@ -1,22 +1,13 @@
 package com.example.hw.controller;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -29,11 +20,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.hw.UserSession;
 import com.example.hw.entity.Category;
 import com.example.hw.entity.Post;
-import com.example.hw.entity.Weather;
 import com.example.hw.infrastructure.dao.CategoryDao;
 import com.example.hw.infrastructure.dao.PostDao;
 import com.example.hw.repository.CategoryRepository;
@@ -148,6 +139,10 @@ public class PostController {
 		Page<Post> postPage;
 		Double degree = 0.0;
 
+
+String result = openweather();
+
+		
 		if (categoryId.isPresent()) {
 			postPage = postRepository.getPostList(pageable, categoryId.get());
 			model.addAttribute("category", categoryRepository.getCategory(categoryId.get()).getName());
@@ -155,118 +150,30 @@ public class PostController {
 			postPage = postRepository.getPostList(pageable);
 		}
 
-		model.addAttribute("Weather", Weather());
-		model.addAttribute("Degree", Degree(degree));
+		JSONObject obj = null;
+		try {
+			obj = new JSONObject(result);
+			model.addAttribute("icon", obj.getJSONArray("weather").getJSONObject(0).getString("icon"));
+			model.addAttribute("temp", obj.getJSONObject("main").getLong("temp"));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
+		
 		model.addAttribute("postPage", postPage);
 
 		return "list";
 	}
 
-	public String Degree(Double Degree) {
-		// http://api.openweathermap.org/data/2.5/weather?q=seoul&APPID=a0678fc18cd7e7537688102eef6793bb
-		URL url;// URL 주소 객체
-		URLConnection connection;// URL접속을 가지는 객체
-		InputStream is;// URL접속에서 내용을 읽기 위한 Stream
-		InputStreamReader isr;
-		BufferedReader br;
+	public String openweather() {
+		final String uri = "http://api.openweathermap.org/data/2.5/weather?units=metric&q=seoul&APPID=a0678fc18cd7e7537688102eef6793bb";
 
-		try {
-			// URL객체를 생성하고 해당 URL로 접속한다..
-			url = new URL(
-					"http://api.openweathermap.org/data/2.5/weather?q=seoul&APPID=a0678fc18cd7e7537688102eef6793bb");
-			connection = url.openConnection();
-			// 내용을 읽어오기위한 InputStream객체를 생성한다.
-			is = connection.getInputStream();
-			isr = new InputStreamReader(is);
-			br = new BufferedReader(isr);
+		RestTemplate restTemplate = new RestTemplate();
+		String result = restTemplate.getForObject(uri, String.class);
 
-			String buf = null;
-			String json = ""; // JSON 결과를 String 생성
+		return result;
 
-			while (true) {
-				buf = br.readLine();
-				if (buf == null)
-					break;
-				json = buf; // JSON 결과 저장
-			}
-
-			System.out.println(json);
-
-			JSONParser parser = new JSONParser();
-			JSONObject temp1 = (JSONObject) parser.parse(json);
-			JSONObject temp2 = (JSONObject) temp1.get("main");
-
-			Degree = (double) temp2.get("temp") - 273.15;
-
-		} catch (MalformedURLException mue) {
-			// TODO Auto-generated catch block
-			System.err.println("잘못된 URL입니다.");
-			System.exit(1);
-		} catch (IOException ioe) {
-			// TODO Auto-generated catch block
-			System.err.println("IOException " + ioe);
-			ioe.printStackTrace();
-			System.exit(1);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return String.format("%.1f 'C", Degree);
 	}
 
-	public String Weather() {
-		URL url;// URL 주소 객체
-		URLConnection connection;// URL접속을 가지는 객체
-		InputStream is;// URL접속에서 내용을 읽기 위한 Stream
-		InputStreamReader isr;
-		BufferedReader br;
-		String weather = "null";
-		try {
-			// URL객체를 생성하고 해당 URL로 접속한다..
-			url = new URL(
-					"http://api.openweathermap.org/data/2.5/weather?q=seoul&APPID=a0678fc18cd7e7537688102eef6793bb");
-			connection = url.openConnection();
-			// 내용을 읽어오기위한 InputStream객체를 생성한다.
-			is = connection.getInputStream();
-			isr = new InputStreamReader(is);
-			br = new BufferedReader(isr);
-
-			String buf = null;
-			String json = ""; // JSON 결과를 String 생성
-
-			while (true) {
-				buf = br.readLine();
-				if (buf == null)
-					break;
-				json = buf; // JSON 결과 저장
-			}
-
-			System.out.println(json);
-
-			JSONParser parser = new JSONParser();
-			JSONObject temp1 = (JSONObject) parser.parse(json);
-			JSONArray temp2 = (JSONArray) temp1.get("weather");
-
-			temp1 = (JSONObject) temp2.get(0);
-
-			weather = (String) temp1.get("main");
-
-		} catch (MalformedURLException mue) {
-			// TODO Auto-generated catch block
-			System.err.println("잘못된 URL입니다.");
-			System.exit(1);
-		} catch (IOException ioe) {
-			// TODO Auto-generated catch block
-			System.err.println("IOException " + ioe);
-			ioe.printStackTrace();
-			System.exit(1);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return weather;
-	}
 }
